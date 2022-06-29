ec9e4b61594762853b363f0accaa804c335ee37b
VaJoy Larn <vajoy@qq.com>
2022-06-29 19:07:11 +0800

    fix

--- src/js/business.js
+++ src/js/business.js
@@ -13,6 +13,8 @@ export const petCompSetup = (initAnimate) => {
             initAnimate(animateCanvas.value);
             const staticCtx = staticCanvas.value.getContext('2d');
             setTimeout(() => {
+                staticCanvas.value.width = animateCanvas.value.width;
+                staticCanvas.value.height = animateCanvas.value.height;
                 staticCtx.drawImage(animateCanvas.value, 0, 0);
             }, 300)
         });
